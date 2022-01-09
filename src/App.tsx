5959e09671bdd4b22f409c1394b8dbc7338ed93f
matheus-w-a <matheus.w.a@outlook.com.br>
2022-01-09 10:39:48 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { Header } from './components/Header';
 
 interface GenreResponseProps {
   id: number;
@@ -60,35 +63,14 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
+      <SideBar 
+        selectedGenreId={selectedGenreId}
+        handleClickButton={handleClickButton}
+        genres={genres}
+      />
       <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Header selectedGenre={selectedGenre} />
+        <Content movies={movies}/>
       </div>
     </div>
   )
